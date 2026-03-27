189e6e8ecb6cedc1229e3a0188b9ff3e6483dd5e
Parth Patil <parthpatil7589@gmail.com>
2026-03-27 22:47:51 +0530

    Fixed invisible marker icon

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -6,6 +6,15 @@ import { User } from '../types';
 import BottomNavigation from '../components/BottomNavigation';
 import L from 'leaflet'; // Added this
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+L.Marker.prototype.options.icon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41]
+});
 
 // --- ADD THIS BLOCK TO FIX INVISIBLE MARKER ---
 import icon from 'leaflet/dist/images/marker-icon.png';
